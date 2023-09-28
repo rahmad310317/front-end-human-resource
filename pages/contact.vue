45582f7707f2b9971f7a67638d9966a34978b8c2
rahmad310317 <rahmadwindisimanullang@gmail.com>
2023-09-28 12:26:05 +0700

    update setup

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,4 +1,6 @@
 <script setup>
+
+
 definePageMeta({
   layout: false,
 });
@@ -7,11 +9,12 @@ definePageMeta({
 <template>
   <div>
     <NuxtLayout name="custom">
-    <div>ini page contact</div>
-    <template #header>
-      <div class="text-5xl font-bold text-red-500">
-        ini header yang sudah diganti
-      </div>
-    </template>
-  </NuxtLayout></div>
+      <div>ini page contact</div>
+      <template #header>
+        <div class="text-5xl font-bold text-red-500">
+          ini header yang sudah diganti
+        </div>
+      </template>
+    </NuxtLayout>
+  </div>
 </template>
