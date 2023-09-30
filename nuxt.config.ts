6c9f5c307a015aba0653d7688829158271795b7c
rahmad310317 <rahmadwindisimanullang@gmail.com>
2023-09-30 11:15:08 +0700

    update setup nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  
+
+  app: {
+    head: {
+      title: "Human Resources",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          hid: "description",
+          name: "description",
+          content: process.env.npm_package_description || "",
+        },
+      ],
+    },
+  },
+  modules: [
+    "@nuxtjs/axios",
+    "@nuxtjs/auth",
+    "@nuxtjs/dotenv",
+    "@nuxtjs/sitemap",
+    "@nuxtjs/tailwind",
+  ],
 });
