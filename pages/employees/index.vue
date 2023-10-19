0a8bea57be28e8948f81bd75552b9bca9d850641
rahmad310317 <rahmadwindisimanullang@gmail.com>
2023-10-20 00:18:12 +0700

    updatte nuxtlink to employees

--- pages/employees/index.vue
+++ pages/employees/index.vue
@@ -55,8 +55,8 @@ definePageMeta({
               <div class="text-xl font-medium text-dark">Statistics</div>
               <p class="text-grey">Your team powers</p>
             </div>
-            <a href="employee_create.html" class="btn btn-primary"
-              >Add Employee</a
+            <NuxtLink :to="{ name: 'employees-create' }" class="btn btn-primary"
+              >Add Employee</NuxtLink
             >
           </div>
         </div>
--- pages/roles/index.vue
+++ pages/roles/index.vue
@@ -1,6 +1,6 @@
 <script setup>
 definePageMeta({
-  layout: "form",
+  layout: "dashboard",
 });
 </script>
 
