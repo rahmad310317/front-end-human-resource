156a4cc74f73d7d78f26b93a27aa105d7364e1a9
rahmad310317 <rahmadwindisimanullang@gmail.com>
2023-10-30 06:17:39 +0700

    update fetch  API Teams

--- pages/companies/_id/teams/index.vue
+++ pages/companies/_id/teams/index.vue
@@ -1,6 +1,20 @@
 <script>
 export default {
   layout: "dashboard",
+
+  data() {
+    return {
+      teams: [],
+    };
+  },
+  async fetch() {
+    this.teams = await this.$axios.get("/team", {
+      params: {
+        company_id: 1,
+        limit: 100,
+      },
+    });
+  },
 };
 </script>
 
@@ -67,42 +81,26 @@ export default {
             >
           </div>
         </div>
-
         <div
           class="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4 xl:gap-10 lg:gap-3"
         >
-          <div class="items-center card py-6 md:!py-10 md:!px-[38px] !gap-y-0">
+          <p v-if="$fetchState.pending">Fetching Teams...</p>
+          <div
+            class="items-center card py-6 md:!py-10 md:!px-[38px] !gap-y-0"
+            v-else
+            v-for="team in teams.data.data.data"
+          >
             <a
               href="#"
               class="absolute inset-0 focus:ring-2 ring-primary rounded-[26px]"
             ></a>
-            <img src="/assets/svgs/ric-box.svg" alt="" />
+            <img :src="team.icon" alt="" />
             <div class="mt-6 mb-1 font-semibold text-center text-dark">
-              Growth Marketing
+              {{ team.name }}
             </div>
-            <p class="text-center text-grey">12 People</p>
-          </div>
-          <div class="items-center card py-6 md:!py-10 md:!px-[38px] !gap-y-0">
-            <a
-              href="#"
-              class="absolute inset-0 focus:ring-2 ring-primary rounded-[26px]"
-            ></a>
-            <img src="/assets/svgs/ric-target.svg" alt="" />
-            <div class="mt-6 mb-1 font-semibold text-center text-dark">
-              User Growth
-            </div>
-            <p class="text-center text-grey">5,312 People</p>
-          </div>
-          <div class="items-center card py-6 md:!py-10 md:!px-[38px] !gap-y-0">
-            <a
-              href="#"
-              class="absolute inset-0 focus:ring-2 ring-primary rounded-[26px]"
-            ></a>
-            <img src="/assets/svgs/ric-award.svg" alt="" />
-            <div class="mt-6 mb-1 font-semibold text-center text-dark">
-              Gamification
-            </div>
-            <p class="text-center text-grey">893 People</p>
+            <p class="text-center text-grey">
+              People {{ team.employees_count }}
+            </p>
           </div>
         </div>
       </section>
