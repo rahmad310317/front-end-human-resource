eb3ca9cf9cba3618e358ba017a8c8992cd02dffa
rahmad310317 <rahmadwindisimanullang@gmail.com>
2023-11-07 01:24:33 +0700

    update create  employess

--- pages/companies/_id/employees/team.vue
+++ pages/companies/_id/employees/team.vue
@@ -6,15 +6,23 @@
         Add your new people to grow the <br />
         company reaching their goals
       </p>
-      <div class="w-full card !max-w-[560px] sm:!flex-row items-center justify-between">
+      <div
+        class="w-full card !max-w-[560px] sm:!flex-row items-center justify-between"
+      >
         <div class="flex flex-row items-center gap-4">
           <img src="/assets/images/user-f-1.png" width="70" alt="" />
           <div>
-            <div class="text-lg font-semibold">Andini Danna</div>
-            <p class="text-base text-grey">ke@manasihhbang.com</p>
+            <div class="text-lg font-semibold">
+              {{ this.$store.state.employees.name }}
+            </div>
+            <p class="text-base text-grey">
+              {{ this.$store.state.employees.email }}
+            </p>
           </div>
         </div>
-        <p class="text-right text-grey">Product Designer</p>
+        <p class="text-right text-grey">
+          {{ this.$store.state.employees.role_name }}
+        </p>
       </div>
 
       <!-- Your Teams -->
@@ -30,46 +38,27 @@
         </div>
 
         <form>
-          <div class="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4 xl:gap-10 lg:gap-3 mb-[50px]">
-            <div class="items-center card py-6 md:!py-10 md:!px-[38px] !gap-y-0">
-              <input type="radio" name="productGrowth" id="productGrowth"
-                class="absolute inset-0 checked:ring-2 ring-primary rounded-[26px] appearance-none" />
+          <div
+            class="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4 xl:gap-10 lg:gap-3 mb-[50px]"
+          >
+            <div
+              class="items-center card py-6 md:!py-10 md:!px-[38px] !gap-y-0"
+            >
+              <input
+                type="radio"
+                name="productGrowth"
+                id="productGrowth"
+                class="absolute inset-0 checked:ring-2 ring-primary rounded-[26px] appearance-none"
+              />
               <img src="/assets/svgs/ric-box.svg" alt="" />
               <div class="mt-6 mb-1 font-semibold text-center text-dark">
                 Product Growth
               </div>
               <p class="text-center text-grey">810 People</p>
             </div>
-            <div class="items-center card py-6 md:!py-10 md:!px-[38px] !gap-y-0">
-              <input type="radio" name="marketing" id="marketing"
-                class="absolute inset-0 checked:ring-2 ring-primary rounded-[26px] appearance-none" />
-              <img src="/assets/svgs/ric-target.svg" alt="" />
-              <div class="mt-6 mb-1 font-semibold text-center text-dark">
-                Marketing
-              </div>
-              <p class="text-center text-grey">15,810 People</p>
-            </div>
-            <div class="items-center card py-6 md:!py-10 md:!px-[38px] !gap-y-0">
-              <input type="radio" name="globalization" id="globalization"
-                class="absolute inset-0 checked:ring-2 ring-primary rounded-[26px] appearance-none" />
-              <img src="/assets/svgs/ric-globe.svg" alt="" />
-              <div class="mt-6 mb-1 font-semibold text-center text-dark">
-                Globalization
-              </div>
-              <p class="text-center text-grey">300 People</p>
-            </div>
-            <div class="items-center card py-6 md:!py-10 md:!px-[38px] !gap-y-0">
-              <input type="radio" name="gamification" id="gamification"
-                class="absolute inset-0 checked:ring-2 ring-primary rounded-[26px] appearance-none" />
-              <img src="/assets/svgs/ric-award.svg" alt="" />
-              <div class="mt-6 mb-1 font-semibold text-center text-dark">
-                Gamification
-              </div>
-              <p class="text-center text-grey">25 People</p>
-            </div>
           </div>
           <div class="flex justify-center">
-            <button type="button" href="employees.html" id="continueBtn" class="btn btn-primary">
+            <button type="button" id="continueBtn" class="btn btn-primary">
               Continue
             </button>
           </div>
@@ -83,5 +72,16 @@
 export default {
   layout: "form",
   middleware: "auth",
+
+  computed: {
+    team_id() {
+      return this.$store.state.employees.team_id;
+    },
+    methods: {
+      updateTeam_id(event) {
+        this.$store.state.commit("employees/updateTeam", event.target.value);
+      },
+    },
+  },
 };
 </script>
